00b64025b79238db1bdb41023e9e120e8328824c
rjcrter11 <rjcrter11@gmail.com>
2020-03-23 07:50:54 -0500

    dependencies installed, localhost running, knexfile built

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.send("Node auth1 Project");
+});
+
+module.exports = server;
